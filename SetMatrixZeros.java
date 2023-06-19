6bc6169b6e26c71e76eb35eaab68a899cad1337c
Akanksha Mogal <75826303+akankshamogal09@users.noreply.github.com>
2023-06-19 09:12:39 +0530

    Update SetMatrixZeros.java

--- SetMatrixZeros.java
+++ SetMatrixZeros.java
@@ -1,1 +1,17 @@
 
+import java.util.Scanner;
+
+public class SetMatrixZeros{
+public static void main(String args[]){
+  Scanner sc=new Scanner(System.in);
+  int n=sc.nextInt();
+  int m=sc.nextInt();
+  int arr[][]=new arr[n][m];
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      arr[i][j]=sc.nextInt();
+    }
+  }
+  
+}
+}
